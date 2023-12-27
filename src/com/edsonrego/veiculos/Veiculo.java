d6918dbe775ef9304500a7ff8c2a6ea3248c2ec4
EdsonRego <edsonxrego@gmail.com>
2023-12-27 15:23:13 -0300

    Classe Veiculo

---
+++ src/com/edsonrego/veiculos/Veiculo.java
@@ -0,0 +1,11 @@
+package com.edsonrego.veiculos;
+
+public class Veiculo {
+    public void ligar() {
+        System.out.println("Ligando veiculo... \n");
+    }
+
+    public void desligar() {
+        System.out.println("Desligando veiculo... \n");
+    }
+}
